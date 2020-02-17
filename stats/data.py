8a9b32d46bd93bf175b37376f8355ad974261a55
Weidong Sun <sunweidong90@gmail.com>
2020-02-16 23:27:46 -0600

    update variable

--- stats/data.py
+++ stats/data.py
@@ -7,12 +7,13 @@ game_files.sort()
 
 game_frames = []
 for game_file in game_files:
-    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'mluti6', 'event'])
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3',
+    'multi4', 'multi5', 'mluti6', 'event'])
     game_frames.append(game_frame)
 
 games = pd.concat(game_frames)
 
-games.loc[games['multi5'] == '??', ['multi5']] = ''
+games.loc[games['multi5'] == '??', 'multi5'] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
