a3acc20aad9a46e344058f5e4b8cb9e487ad2a3c
Weidong Sun <sunweidong90@gmail.com>
2020-02-16 23:24:35 -0600

    update variable

--- stats/data.py
+++ stats/data.py
@@ -2,15 +2,16 @@ import os
 import glob
 import pandas as pd
 
-
 games_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 games_files.sort()
 
 game_frames = []
 for game_file in games_files:
-    frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'mluti6', 'event'])
-    game_frames.append(frame)
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'mluti6', 'event'])
+    game_frames.append(game_frame)
+
 games = pd.concat(game_frames)
+
 games.loc[games['multi5'] == '??', ['multi5']] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
